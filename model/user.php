d6260b6e98cf3469754f6fa3e0db8db07256d8c9
Nevena Marjanovic <marjanovicnevena18@gmail.com>
2022-01-27 21:09:58 +0100

    Komentari

--- model/user.php
+++ model/user.php
@@ -1,17 +1,18 @@
 <?php
-
+//prvi put koristimo klasu kao koncept
 class User{
     public $id;
     public $username;
     public $password;
 
+    //konstruktor za klasu
     public function __construct($id=null,$username=null,$password=null)
     {
         $this->id = $id;
         $this->username = $username;
         $this->password = $password;
     }
-
+    //funkcija za logovanje
     public static function logInUser($usr, mysqli $conn)
     {
         $query = "SELECT * FROM user WHERE username='$usr->username' and password='$usr->password'";
